641745472c1b16a133d285366dbcd1add3c37a10
mahim007pranto <mahim.pranto@gmail.com>
2020-10-08 21:11:32 +0600

    Vitória and Her Indecision

---
+++ Beginner/1924.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    string A;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>A;
+        
+    }    
+    cout<<"Ciencia da Computacao\n";
+    return 0;
+}

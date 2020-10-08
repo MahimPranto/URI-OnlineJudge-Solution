c03537fda2adf9b1fdb4b34bccad85175688cda0
mahim007pranto <mahim.pranto@gmail.com>
2020-10-08 20:59:06 +0600

    Line in Array

---
+++ Beginner/1181.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<string>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    float N[12][12],avg;
+    string T;
+    int L,sum=0;
+    
+    cin>>L>>T;
+    for(int i=0;i<12;i++)
+    {
+        for(int j=0;j<12;j++)
+            cin>>N[i][j];
+    }
+    
+    for(int j=0;j<12;j++)
+    {
+        sum+=N[L][j];
+        
+    }       
+    if(T=="S")
+        cout<<sum<<endl;
+    else if(T=="M")
+        cout<<fixed<<setprecision(1)<<sum/12.0<<endl;
+    
+    return 0;
+}

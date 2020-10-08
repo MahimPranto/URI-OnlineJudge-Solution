8bb38e5ebeb0fe6915f1268003fa376aadbca4f9
mahim007pranto <mahim.pranto@gmail.com>
2020-10-08 20:40:24 +0600

    Multiples of 13

---
+++ Beginner/1132.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int X,Y,sum=0;
+    cin>>X>>Y;
+    
+    if(X>=Y)
+    {
+        for(int i= Y;i<=X;i++)
+        {
+            if(i%13!=0)
+                sum+=i;
+        }
+    }
+    else
+    {
+        for(int i= X;i<=Y;i++)
+        {
+            if(i%13!=0)
+                sum+=i;
+        }
+    } 
+    
+    cout<<sum<<endl;
+    return 0;
+
+}

10102871fe1b990ad1c2d7cb81a7bf43693587d7
mahim007pranto <mahim.pranto@gmail.com>
2020-10-08 21:27:56 +0600

    Merry Christmaaas!

---
+++ Beginner/2483.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int I;
+    cin>>I;
+    
+    string A="Feliz nat";
+    
+    for(int i=0;i<I;i++)
+    {
+        A+="a";    
+    }
+    A+="l!";
+    
+    cout<<A<<endl;
+    return 0;
+}

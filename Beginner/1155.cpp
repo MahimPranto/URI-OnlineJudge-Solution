62d6a95f393b6d06c1e0dd55e7a0eeeb6a6045ce
mahim007pranto <mahim.pranto@gmail.com>
2020-10-08 20:49:14 +0600

    S Sequence

---
+++ Beginner/1155.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    float s=0;
+    
+    for(int i=1;i<=100;i++)
+    {
+        s+=1/float(i);
+        
+    }
+    
+    cout<<fixed<<setprecision(2)<<s<<endl;
+    return 0;
+}
